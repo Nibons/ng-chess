a9b12c74d052419f9d9c928cababfe73c01de299
Clay Haertzen <chaertzen@deloitte.com>
2018-05-27 23:01:37 -0400

    added some service stuff

--- src/app/chess-service/game-service.service.ts
+++ src/app/chess-service/game-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GameConfig } from '@chess/GameConfig';
+import { Board } from '@chess/board';
+import { IPlayer } from '@chess/iplayer.model';
 
 @Injectable({
   providedIn: 'root'
