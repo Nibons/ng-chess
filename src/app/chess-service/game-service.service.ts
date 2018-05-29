ef38f8a0c56f4b7f54ac918af98ca5da4c0af0e7
Clay Haertzen <imac5640@gmail.com>
2018-05-29 12:31:37 -0400

    before_clean

--- src/app/chess-service/game-service.service.ts
+++ src/app/chess-service/game-service.service.ts
@@ -1,11 +1,13 @@
 import { IBoardConstructor } from '@chess/i-board-constructor.model';
 import { IPieceConstructor } from '@chess/ipiece-constructor.model';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { GameConfig } from '@chess/GameConfig';
 import { Board } from '@chess/board';
 import { IPlayer } from '@chess/iplayer.model';
 import { Game } from '@chess/game';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 const game_template_directory = 'assets/game_templates';
 
@@ -15,13 +17,13 @@ const game_template_directory = 'assets/game_templates';
 export class GameService {
 
   public gameList: Game[];
+  public playerList: IPlayer[];
 
-  constructor() {
+  constructor(public _http: HttpClient) {
     // this.readGameTypeList();
   }
   newGame(name: string): Game {
-    // const playerList: IPlayer[];
-    return new Game(this.getBoardConfig(name), this.getPieceList(name), playerList);
+    return new Game(this.getBoardConfig(name), this.getPieceList(name), this.playerList);
   }
   saveGame(name: string) {
 
@@ -35,9 +37,26 @@ export class GameService {
   }
 
   private getBoardConfig(name: string): IBoardConstructor {
-    const file = require(`${game_template_directory}/${name}.board.json`);
+    return (<IBoardConstructor>this.getGameTemplateJson(name).subscribe(res => response.json));
+
     return JSON.parse(file);
   }
+
+  // private AsyncgetGameTemplateJson(name: string): Observable<any> {
+  //
+  //   return this._http.get(file);
+  // }
+  // private asyncReadTemplatJson(observable: Observable<any>) {
+  //   const subscription: Subscription = observable.subscribe();
+  // }
+
+  private AsyncGetBoardConfig(name: string): Observable<IBoardConstructor> {
+    const url = require(`${game_template_directory}/${name}.board.json`);
+    return this._http.get(url)
+      .map()
+  }
+
+
 
 }
 
