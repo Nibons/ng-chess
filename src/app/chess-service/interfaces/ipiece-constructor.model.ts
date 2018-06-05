65ab925ad125ea2b10ba2b511877cb8c7940dd47
Clay Haertzen <imac5640@gmail.com>
2018-06-05 09:05:01 -0400

    removed constructor interface models

--- src/app/chess-service/interfaces/ipiece-constructor.model.ts
+++
@@ -1,9 +0,0 @@
-import { EPieceType } from '@chess/e-piece-type.enum';
-import { ICoordinates } from '@chess/icoordinates.model';
-
-export interface IPieceConstructor {
-  playerNumber: number;
-  pieceType: EPieceType;
-  coordinates: ICoordinates;
-  IsPrimary: boolean;
-}
