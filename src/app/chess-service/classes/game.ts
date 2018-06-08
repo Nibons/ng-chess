afaa38fe0355b8800d1988f8944daed61ee9e5eb
Clay Haertzen <imac5640@gmail.com>
2018-06-08 17:59:05 -0400

    adjusted Game

--- src/app/chess-service/classes/game.ts
+++ src/app/chess-service/classes/game.ts
@@ -1,18 +1,28 @@
-import { GameStateModel } from './../interfaces/igame.model';
+import { GameStateModel } from '@chess/igame.model';
 import { IPosition } from '@chess/IPosition.model';
 import { IPlayer } from '@chess/iplayer.model';
 import { IBoard } from '@chess/iboard.model';
 import { IGame } from '@chess/igame.model';
 import { Guid } from '@chess/guid';
+import { Store } from '@ngxs/store';
+import { IPiece } from '@chess/ipiece.model';
 export class Game implements IGame {
-  public Id: Guid = Guid.newGuid();
+  public Id: Guid;
   public IdCounter = 0;
-  constructor({ public Id, public IdCounter }: GameStateModel) {
+  constructor({ Id, IdCounter }: GameStateModel, public store: Store) {
     this.Id = Id;
     this.IdCounter = IdCounter;
   }
-  GetBoardById(boardId: number): IBoard { }
-  GetPlayerById(playerId: number): IPlayer { }
-  GetPositionById(positionId: number): IPosition { }
-  GetPieceById(pieceId: number): IPiece { }
+  GetPlayerById(playerId: number): IPlayer {
+    throw new Error('Method not implemented.');
+  }
+  GetBoardById(boardId: number): IBoard {
+    throw new Error('Method not implemented.');
+  }
+  GetPositionById(positionId: number): IPosition {
+    throw new Error('Method not implemented.');
+  }
+  GetPieceById(pieceId: number): IPiece {
+    throw new Error('Method not implemented.');
+  }
 }
