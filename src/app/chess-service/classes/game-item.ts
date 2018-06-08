611ada85f3b88a450910b591e35f69d86a1f93c2
Clay Haertzen <imac5640@gmail.com>
2018-06-08 17:56:21 -0400

    removed game from GameItem

--- src/app/chess-service/classes/game-item.ts
+++ src/app/chess-service/classes/game-item.ts
@@ -5,9 +5,6 @@ import { GameStateModel, IGame } from '@chess/igame.model';
 
 export abstract class GameItem {
   Id: number;
-  get game(): IGame {
-    return await this.game$;
-  }
   @Select(GameState) game$: GameStateModel;
   constructor(public gameId: Guid, public _store?: Store) {
     this.Id = _store.selectSnapshot<number>(state => state.GameState.IdCounter);
