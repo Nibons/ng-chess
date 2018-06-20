859f8e6a16dc44a27d3e18d13073cad1fc073e2b
Clay Haertzen <imac5640@gmail.com>
2018-06-20 16:59:38 -0400

    adjusted service

--- src/app/chess-service/classes/chess-service.ts
+++ src/app/chess-service/classes/chess-service.ts
@@ -4,7 +4,7 @@ import { PositionStateModel } from '@chess/IPosition.model';
 import { IGameTemplate } from './../interfaces/igame-template.model';
 import { Injectable } from '@angular/core';
 import { IPieceActor } from '@chess/IPieceActor.model';
-import { Store } from '@ngxs/store';
+import { Store, Actions, ofActionSuccessful } from '@ngxs/store';
 import { King } from '@chess/pieces/king';
 import { Queen } from '@chess/pieces/queen';
 import { Rook } from '@chess/pieces/rook';
@@ -15,6 +15,8 @@ import { Observable } from 'rxjs';
 import { PieceState } from '@chess/piece-state';
 import { PositionState } from '@chess/position-state';
 import { BoardState } from '@chess/board-state';
+import { SetGame } from '@chess/SetGame';
+import { CreatePiece } from '@chess/CreatePiece';
 
 @Injectable()
 export class ChessService {
@@ -24,18 +26,29 @@ export class ChessService {
   protected positions$: Observable<PositionStateModel[]> = this.store.select(PositionState.PositionList);
   protected boards$: Observable<BoardStateModel[]> = this.store.select(BoardState.BoardList);
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private actions$: Actions) {
     // create each type of piece actor, that will watch the PieceStateModel for changes
-    this.createPieceActors();
+    // this.createPieceActors();
+    this.actions$.pipe(
+      ofActionSuccessful(SetGame)
+    ).subscribe(() => {
+      this.createPieceActors();
+    });
+
+  }
+  public InvokeWatch() {
+    this.pieceActors.forEach(
+      (actor: IPieceActor) => actor.WatchAllPieces()
+    );
   }
 
-  private createPieceActors(): void {
-    this.pieceActors.push(new King(this.store));
-    this.pieceActors.push(new Queen(this.store));
-    this.pieceActors.push(new Rook(this.store));
-    this.pieceActors.push(new Knight(this.store));
-    this.pieceActors.push(new Bishop(this.store));
-    this.pieceActors.push(new Pawn(this.store));
+  public createPieceActors(): void {
+    this.pieceActors.push(new King(this.store, this.actions$));
+    this.pieceActors.push(new Queen(this.store, this.actions$));
+    this.pieceActors.push(new Rook(this.store, this.actions$));
+    this.pieceActors.push(new Knight(this.store, this.actions$));
+    this.pieceActors.push(new Bishop(this.store, this.actions$));
+    this.pieceActors.push(new Pawn(this.store, this.actions$));
   }
 
 }
