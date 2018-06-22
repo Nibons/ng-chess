7c4f4f81dd3db1da0ee47b76490fad4ec1e49a56
Clay Haertzen <imac5640@gmail.com>
2018-06-22 09:30:45 -0400

    added boardNumber automatic value

--- src/app/chess-service/actions/CreatePiece.ts
+++ src/app/chess-service/actions/CreatePiece.ts
@@ -1,7 +1,13 @@
+import { BoardState } from '@chess/board-state';
 import { Guid } from '@chess/guid';
 import { PieceStateModel } from '@chess/ipiece.model';
 import { Store } from '@ngxs/store';
+
 export class CreatePiece {
   static readonly type = '[Piece] CreatePiece';
-  constructor(public piece: PieceStateModel, public gameId: Guid) { }
+  public boardId: Guid;
+  constructor(public piece: PieceStateModel, store: Store) {
+    const boardNumber = piece.boardNumber ? piece.boardNumber : 0;
+    this.boardId = store.selectSnapshot(BoardState.BoardList).filter(b => b.gameId === piece.gameId)[boardNumber].Id;
+  }
 }
