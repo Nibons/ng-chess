49a17524886ac78367fccf61835d4c88ba627499
Clay Haertzen <imac5640@gmail.com>
2018-06-22 09:25:43 -0400

    adjusted how createBoard works

--- src/app/chess-service/actions/CreateBoard.ts
+++ src/app/chess-service/actions/CreateBoard.ts
@@ -1,18 +1,19 @@
 import { CreateAllPositions } from '@chess/CreateAllPositions';
 import { BoardStateModel } from '@chess/iboard.model';
 import { Store } from '@ngxs/store';
+import { Guid } from '@chess/guid';
 
 export class CreateBoard {
   static readonly type = '[Board] CreateBoard';
   public payload: BoardStateModel;
-  constructor(board: BoardStateModel, store: Store) {
+  constructor(board: BoardStateModel, gameId: Guid, store: Store) {
     this.payload = {
-      gameId: board.gameId,
-      Id: board.Id,
+      gameId: gameId,
+      Id: Guid.newGuid(),
       direction: board.direction,
       range: board.range
     };
     store.dispatch(
-      new CreateAllPositions(board.range, board.Id, board.gameId, store));
+      new CreateAllPositions(this.payload.range, this.payload.Id, gameId, store));
   }
 }
