05dff3b15cbc62ee93c9ff323455aca464828d14
Clay Haertzen <imac5640@gmail.com>
2018-06-22 09:22:50 -0400

    renamed placePiece to SetPieceAtPosition

--- src/app/chess-service/actions/SetPieceAtPosition.ts
+++ src/app/chess-service/actions/SetPieceAtPosition.ts
@@ -1,6 +1,9 @@
+import { PositionStateModel } from './../interfaces/iposition.model';
 import { Guid } from '@chess/guid';
 import { ICoordinates } from '@chess/icoordinates.model';
-export class PlacePiece {
+import { Store } from '@ngxs/store';
+import { PositionState } from '@chess/position-state';
+export class SetPieceAtPosition {
   static readonly type = '[Position] SetPieceAtPosition';
   constructor(public pieceId: Guid, public coordinates: ICoordinates, public boardId: Guid) { }
 }
