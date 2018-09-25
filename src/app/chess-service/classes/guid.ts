f5b2470bb58cebcf215e3f0a9bd4bd255adac420
Clay Haertzen <chaertzen@deloitte.com>
2018-09-24 22:56:20 -0400

    fixed Guid class a bit more

--- src/app/chess-service/classes/guid.ts
+++ src/app/chess-service/classes/guid.ts
@@ -1,11 +1,17 @@
 export class Guid {
-  private constructor(private _guid = Guid.newGuid()) { }
-  static newGuid() {
-    const _string = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+
+  constructor(private _guid: string) { }
+
+  private static GetPrivateStringGuid(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
       // tslint:disable-next-line:no-bitwise
       const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
+  }
+
+  static newGuid(): Guid {
+    const _string = Guid.GetPrivateStringGuid();
     return new Guid(_string);
   }
   static Compare(guid1: Guid, guid2: Guid): boolean {
