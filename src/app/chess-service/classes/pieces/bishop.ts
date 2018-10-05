dad91f57991806c4e635575ea8d083ac51408637
Clay Haertzen <clay.haertzen@e-hps.com>
2018-10-05 17:54:56 -0400

    added some things for IVector

--- src/app/chess-service/classes/pieces/bishop.ts
+++ src/app/chess-service/classes/pieces/bishop.ts
@@ -3,13 +3,15 @@ import { GameService } from 'src/app/chess-service/classes/GameService';
 import { EPieceType } from 'src/app/chess-service/enums/e-piece-type.enum';
 import { BasePiece } from 'src/app/chess-service/classes/BasePiece';
 import { Injectable } from '@angular/core';
+import { IVector } from 'src/app/chess-service/interfaces/ivector';
+import { VectorLibrary } from 'src/app/chess-service/classes/vector';
 
 @Injectable({ providedIn: 'root' })
 export class Bishop extends BasePiece {
   pieceType = EPieceType.bishop;
 
-  static bishopThreat(piece: Piece, distance = Number.MAX_SAFE_INTEGER): void {
-
+  static bishopThreat(piece: Piece, distance = Number.MAX_SAFE_INTEGER): IVector[] {
+    return VectorLibrary.GetDiagonalDirectionVectorList(piece.coordinates, distance);
   }
 
   constructor(public gameService: GameService) {
--- src/app/chess-service/state/board/board.query.ts
+++ src/app/chess-service/state/board/board.query.ts
@@ -3,8 +3,6 @@ import { QueryEntity, ID } from '@datorama/akita';
 import { BoardStore, BoardState } from './board.store';
 import { Board } from './board.model';
 import { PositionQuery } from 'src/app/chess-service/state/position';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class BoardQuery extends QueryEntity<BoardState, Board> {
@@ -13,4 +11,8 @@ export class BoardQuery extends QueryEntity<BoardState, Board> {
   constructor(protected store: BoardStore, private positionQuery: PositionQuery) {
     super(store);
   }
+
+  // GetPositionsUntilOccupied$(vector: IVector, boardId: ID): Observable<Position[]> {
+  //   this.positionQuery;
+  // }
 }
