8af1c808710c7d10aef1e01f5b54d8ff8150fd0c
Clay Haertzen <clay.haertzen@e-hps.com>
2018-10-10 10:01:27 -0400

    cleared up import list

--- src/app/chess-service/state/combined.query.ts
+++ src/app/chess-service/state/combined.query.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
-import { QueryEntity, ID } from '@datorama/akita';
-import { Observable, from, of } from 'rxjs';
-import { getEntityByObservableId$ } from 'src/app/chess-service/state/shared/shared.query';
-import { mergeMap, map } from 'rxjs/operators';
-import { PieceQuery } from 'src/app/chess-service/state/piece';
-import { PositionQuery } from 'src/app/chess-service/state/position';
-import { ICoordinates } from 'src/app/chess-service/interfaces/icoordinates.model';
-import { BoardQuery } from 'src/app/chess-service/state/board';
+
+import { PieceQuery } from './piece';
+import { PositionQuery } from './position';
+import { BoardQuery } from './board';
 
 @Injectable({ providedIn: 'root' })
 export class CombinedQuery {
 
   constructor(
-    private boardQuery: BoardQuery,
-    private piecesQuery: PieceQuery,
-    private positionQuery: PositionQuery
+    protected pieceQuery: PieceQuery,
+    protected positionQuery: PositionQuery,
+    protected boardQuery: BoardQuery
   ) { }
 }
