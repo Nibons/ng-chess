23eb15e8d3352cef78d53633435eb6a18f4d245b
Clay Haertzen <clay.haertzen@e-hps.com>
2018-10-23 13:53:30 -0400

    simplified game.service

--- src/app/chess-service/state/game/game.service.ts
+++ src/app/chess-service/state/game/game.service.ts
@@ -3,9 +3,7 @@ import { GameStore } from './game.store';
 import { Game, createGame } from 'src/app/chess-service/state/game/game.model';
 import { BoardService } from 'src/app/chess-service/state/board/board.service';
 
-import { GameQuery } from 'src/app/chess-service/state/game/game.query';
 import { PieceService } from 'src/app/chess-service/state/piece/piece.service';
-import { createBoard } from 'src/app/chess-service/state/board/board.model';
 import { Gamesave } from 'src/app/chess-service/state/gamesave';
 import { ID } from '@datorama/akita';
 
@@ -14,7 +12,6 @@ export class GameService {
 
   constructor(
     private gameStore: GameStore,
-    private gameQuery: GameQuery,
     @Inject(forwardRef(() => BoardService)) private boardService: BoardService,
     @Inject(forwardRef(() => PieceService)) private pieceService: PieceService
   ) { }
@@ -25,21 +22,17 @@ export class GameService {
     // });
   }
 
-  add(game: Game) {
-    // adds the game to the game service
-    this.gameStore.setLoading(true);
+  private add(game: Game) {
     this.gameStore.add(game);
-    game.template.boards.forEach(
-      boardTemplate =>
-        this.boardService.add(createBoard(boardTemplate, game.id))
-    );
-    this.pieceService.populateAllPieces(game.id);
-    this.gameStore.setLoading(false);
   }
 
   createFromGameSave(gameSave: Gamesave): ID {
+    this.gameStore.setLoading(true);
     const newGame = createGame(gameSave);
     this.add(newGame);
+    this.boardService.createBoardsFromGame(newGame.id);
+    this.pieceService.populateAllPieces(newGame.id);
+    this.gameStore.setLoading(false);
     return newGame.id;
   }
 }
