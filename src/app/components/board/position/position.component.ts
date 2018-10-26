03266ecb5994663f5b25e1fa2bbc8ad7143fd406
Clay Haertzen <chaertzen@deloitte.com>
2018-10-26 00:20:58 -0400

    refactored position component

--- src/app/components/board/position/position.component.ts
+++ src/app/components/board/position/position.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ID } from '@datorama/akita';
 import { PositionQuery, Position } from 'src/app/chess-service/state/position';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { ICoordinates } from 'src/app/chess-service/interfaces/icoordinates.model';
-import { map } from 'rxjs/operators';
+import { map, reduce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-position',
@@ -14,45 +14,41 @@ export class PositionComponent implements OnInit {
   @Input() positionCoords: ICoordinates = { dimensions: [] };
   @Input() boardId: ID = 0;
 
-  positionId$: Observable<ID>;
-  position$: Observable<Position>;
+  position$: Observable<Position> =
+    this.positionQuery.selectPositionByCoordinates$(this.positionCoords, this.boardId);
+  positionId$: Observable<ID> = this.position$.pipe(map(p => p.id));
 
 
-  coordinates$(): Observable<ICoordinates> {
-    return this.position$.pipe(
-      map(position => position.coordinates)
-    );
-  }
-  isEmpty$(): Observable<boolean> {
-    return this.position$.pipe(
-      map(position => position.pieceId === null)
-    );
-  }
-  isOccupied$(): Observable<boolean> { return this.isEmpty$().pipe(map(isEmpty => !isEmpty)); }
-  pieceId$(): Observable<ID | null> { return this.position$.pipe(map(position => position.pieceId)); }
-  isSelected$(): Observable<boolean> {
-    return this.position$.pipe(map(position => position.selected));
-  }
+  isOccupied$: Observable<boolean> = this.position$.pipe(
+    map(position => position.pieceId !== null)
+  );
+  isEmpty$: Observable<boolean> = this.isOccupied$.pipe(map(b => !b));
 
-  isShaded$(): boolean {
 
+  coordinates$: Observable<ICoordinates> = this.position$.pipe(
+    map(position => position.coordinates)
+  );
+
+  isSelected$: Observable<boolean> =
+    this.position$.pipe(map(position => position.selected));
+
+  pieceId$: Observable<ID | null> =
+    this.position$.pipe(map(position => position.pieceId));
+
+  isShaded$: Observable<boolean> = from(this.positionCoords.dimensions).pipe(
     // | |x|
     // |x| |
     // ex. [0,0] = true
     // ex. [0,1] = false
     // ex. [1,0] = false
     // ex. [1,1] = true
+    reduce(((acc, val) => acc + val), 0),
+    map(value => (value % 2) === 0)
+  );
 
-    let iterate = 0;
-    this.positionCoords.dimensions.forEach(n => iterate = iterate + n);
-    return ((iterate % 2) === 0);
-  }
-
-  constructor(protected positionQuery: PositionQuery) {
-    this.position$ = positionQuery
-      .positionByCoordinates$(this.positionCoords, this.boardId);
-    this.positionId$ = this.position$.pipe(map(p => p.id));
-  }
+  constructor(
+    protected positionQuery: PositionQuery
+  ) { }
 
   ngOnInit() { }
 
