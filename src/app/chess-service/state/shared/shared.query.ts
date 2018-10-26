9b764ce17a77bd7439a64cbf726efe1914042e51
Clay Haertzen <chaertzen@deloitte.com>
2018-10-26 00:16:30 -0400

    added rxjs-spy

--- src/app/chess-service/state/shared/shared.query.ts
+++ src/app/chess-service/state/shared/shared.query.ts
@@ -2,6 +2,7 @@ import { ID } from '@datorama/akita';
 import { Observable, from } from 'rxjs';
 import { mergeMap, withLatestFrom, filter, map } from 'rxjs/operators';
 import { IQueryableById } from 'src/app/chess-service/state/shared/queryableById.model';
+import { tag } from 'rxjs-spy/operators';
 
 
 export function getEntityByObservableId$<T extends IQueryableById>(
@@ -12,9 +13,7 @@ export function getEntityByObservableId$<T extends IQueryableById>(
     mergeMap(itemList$ => from(itemList$)),
     withLatestFrom(itemId),
     filter(([item, id]) => item.id === id),
-    map(([item, id]) => {
-      console.log(`Found item with Id: ` + id);
-      return item;
-    })
+    map(([item, id]) => item),
+    tag('entity')
   );
 }
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+// import { create as create_spy } from 'rxjs-spy';
+
+// export const spy = create_spy()
+//   .log('board*'); // logs all observables with tag
 
 // import { RouterModule } from '@angular/router';
-
 // import { routes as rootRoutes } from './app.routes';
 
 @NgModule({
