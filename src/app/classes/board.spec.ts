cde8c5cbbcb4636afd29d976f5b11838bee5a12c
Clay Haertzen <imac5640@gmail.com>
2019-06-17 03:09:45 -0400

    not enough positions are getting made- test will verify

--- src/app/classes/board.spec.ts
+++ src/app/classes/board.spec.ts
@@ -2,8 +2,10 @@ import { Board } from './board';
 
 describe('Board', () => {
   it('should create the default', () => {
+    const board = Board.create(Board.defaultBoardMin, Board.defaultBoardMax, 0);
     expect(
-      Board.create(Board.defaultBoardMin, Board.defaultBoardMax, 0)
+      board
     ).toBeTruthy();
+    expect(board.positions.length).toEqual(64);
   });
 });
